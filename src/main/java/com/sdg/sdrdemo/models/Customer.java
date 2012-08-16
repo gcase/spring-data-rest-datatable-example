3f0c52bfc2cea62c3fc16643aae610ba225470aa
Greg Case <greg.case@gmail.com>
2012-08-15 23:17:56 -0500

    Formatting

--- src/main/java/com/sdg/sdrdemo/models/Customer.java
+++ src/main/java/com/sdg/sdrdemo/models/Customer.java
@@ -10,38 +10,45 @@ import org.springframework.data.rest.repository.annotation.RestResource;
 @Entity
 @RestResource
 public class Customer {
-	
+
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long customerId;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long customerId;
 
 	private String name;
 	private String email;
 	private String favoriteColor;
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getFavoriteColor() {
 		return favoriteColor;
 	}
+
 	public void setFavoriteColor(String favoriteColor) {
 		this.favoriteColor = favoriteColor;
 	}
+
 	public Long getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(Long customerId) {
 		this.customerId = customerId;
 	}
-	
+
 }
